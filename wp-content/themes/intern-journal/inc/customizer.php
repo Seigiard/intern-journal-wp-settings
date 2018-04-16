7142af7bc87774eb876aa308b427c707a3e4140f
Andrew Borisenko <seigiard@gmail.com>
2018-04-16 22:01:48 +0300

    ADD basic settings

---
+++ wp-content/themes/intern-journal/inc/customizer.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Intern Journal Theme Customizer
+ *
+ * @package Intern_Journal
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function intern_journal_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial( 'blogname', array(
+			'selector'        => '.site-title a',
+			'render_callback' => 'intern_journal_customize_partial_blogname',
+		) );
+		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+			'selector'        => '.site-description',
+			'render_callback' => 'intern_journal_customize_partial_blogdescription',
+		) );
+	}
+}
+add_action( 'customize_register', 'intern_journal_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function intern_journal_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function intern_journal_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function intern_journal_customize_preview_js() {
+	wp_enqueue_script( 'intern-journal-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'intern_journal_customize_preview_js' );
